26b81bf48ccbba52c7f6bb0835786b4ea0248391
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-04 20:28:08 -0600

    Update 20505.java

--- Chapter 1/20505.java
+++ Chapter 1/20505.java
@@ -1,1 +1,5 @@
+/*
+Suppose your name was Alan Turing. Write a statement that would print your last name, followed by a comma, followed by a space and your first name.
+*/
+
 System.out.println("Turing, Alan");
