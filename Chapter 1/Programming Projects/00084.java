2939e5afe0abc15c32eb6c3d588f79d3e4da5e2e
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-04 21:15:32 -0600

    Create 00084.java

---
+++ Chapter 1/Programming Projects/00084.java
@@ -0,0 +1,19 @@
+/*
+(Physics: acceleration)
+
+Average acceleration is defined as the change of velocity divided by the time taken to make the change, as shown in the following formula:
+
+a = (v1 - v0) / t
+
+Here, v0 is the starting velocity in meters/second, v1 is the ending velocity in meters/second, and t is the time span in seconds. Assume v0 is 5.6, v1 is 10.5, and t is 0.5, and write the code to display the average acceleration. 
+
+Class Name: Exercise01_02Extra
+
+If you get a logical or runtime error, please refer https://liveexample.pearsoncmg.com/faq.html.
+*/
+
+public class Exercise01_02Extra{
+	public static void main(String[] args) {
+		System.out.println((10.5 - 5.6) / 0.5);
+	}
+}
