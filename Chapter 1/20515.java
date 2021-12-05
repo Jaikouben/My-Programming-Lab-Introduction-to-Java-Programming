c267fd18b5058257adf32c6e4234dbb230cda952
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-04 20:26:55 -0600

    Create 20515.java

---
+++ Chapter 1/20515.java
@@ -0,0 +1,9 @@
+/*
+Write a complete program whose class name is Hello and that displays Hello, world on the screen.  
+*/
+
+public class Hello {
+	public static void main (String[] args){
+		System.out.println("Hello, world");
+	}
+}
