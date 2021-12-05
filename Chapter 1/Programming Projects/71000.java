da1d3e3a017d0250f40904954b20cf594150921f
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-04 20:55:09 -0600

    Create 71000.java

---
+++ Chapter 1/Programming Projects/71000.java
@@ -0,0 +1,23 @@
+/*
+Chapter 1 Exercise 2
+
+Welcome already.
+(Display five messages) Write a program that displays Welcome to Java five times.
+
+
+INPUT and PROMPTS. None.
+
+OUTPUT. The output should be five lines of the above-described welcome message.
+
+CLASS NAMES. Your program class should be called WelcomeTimes5
+*/
+
+public class WelcomeTimes5 {
+	public static void main(String[] args){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+	}
+}
