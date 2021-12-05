2e6d3816e8a9b55eeef97ccc8bb77d34e3249acb
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-04 20:27:28 -0600

    Update 20506.java

--- Chapter 1/20506.java
+++ Chapter 1/20506.java
@@ -1,3 +1,7 @@
+/*
+Suppose your name was George Gershwin. Write a complete main method that would print your last name, followed by a comma, followed by a space and your first name.
+*/
+
 public static void main(String[] args) {
 	System.out.println("Gershwin, George");
 }
