15b9922eff3a1658bbd76f449bf77cad42fcff68
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:37:47 -0600

    Create 20532.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20532.java
@@ -0,0 +1,5 @@
+/*
+Given the variables fullAdmissionPrice and discountAmount (already declared and assigned values), write an expression corresponding to the price of a discount admission.
+*/
+
+fullAdmissionPrice - discountAmount
