6f53a6319db49a055adcdca2a70154a507e5094f
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 14:56:28 -0600

    Create 20508.java

---
+++ Chapter 2/2.2 Writing a Simple Program/20508.java
@@ -0,0 +1,7 @@
+/*
+Given a floating point variable fraction, write a statement that displays the value of fraction on the screen.
+
+Do not display anything else on the screen-- just the value of fraction.
+*/
+
+System.out.println(fraction);
