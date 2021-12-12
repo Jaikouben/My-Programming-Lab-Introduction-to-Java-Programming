c4cda473bd2a2c07f154575abaed132a96e049ca
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:33:30 -0600

    Create 20528.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20528.java
@@ -0,0 +1,13 @@
+/*
+Declare and initialize the following variables:
+
+  - monthOfYear, initialized to the value 11
+  - companyRevenue, initialized to the value 5666777
+  - firstClassTicketPrice, initialized to the value 6000
+  - totalPopulation, initialized to the value 1222333
+*/
+
+int monthOfYear = 11;
+int companyRevenue = 5666777;
+int firstClassTicketPrice = 6000;
+int totalPopulation = 1222333;
