78fb586de45acaf2a7713c95971114e75ed74815
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:50:26 -0600

    Create 20983.java

---
+++ Chapter 2/2.12 Evaluating Expressions and Operator Precedence/20983.java
@@ -0,0 +1,5 @@
+/*
+Each of the walls of a room with square dimensions has been built with two pieces of sheetrock, a smaller one and a larger one. The length of all the smaller ones is the same and is stored in the variable small. Similarly, the length of all the larger ones is the same and is stored in the variable large. Write a single expression whose value is the total floor-space area of this room. DO NOT USE any method invocations.
+*/
+
+small * large
