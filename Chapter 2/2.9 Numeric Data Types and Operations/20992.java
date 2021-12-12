94cfc4da97ced05cd8ff4edf2ede9bb827e3f40a
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:30:27 -0600

    Create 20992.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20992.java
@@ -0,0 +1,7 @@
+/*
+Assume that an int variable x that has already been declared, and initialized to a non-negative value.
+
+Write an expression whose value is the last (rightmost) digit of x.
+*/
+
+x % 10
