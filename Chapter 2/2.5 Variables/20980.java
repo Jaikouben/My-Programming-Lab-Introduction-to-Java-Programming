c4d833678d82ebae1e3cb0e1a7efc39d37afb4a8
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:21:43 -0600

    Create 20980.java

---
+++ Chapter 2/2.5 Variables/20980.java
@@ -0,0 +1,5 @@
+/*
+Write a single statement that will print the message "first is " followed by the value of first, and then a space, followed by "second = ", followed by the value of second. Print everything on one line and go to a new line after printing. Assume that first has already been declared as a double and that second has been declared as an int. Assume also that the variables have already been given values.
+*/
+
+System.out.println("first is " + first + " second = " + second + "\n");
