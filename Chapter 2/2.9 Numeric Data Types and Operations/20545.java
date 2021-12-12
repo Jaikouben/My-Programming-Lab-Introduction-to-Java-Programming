845f3738f473740bbc7e66d6204f5068c09e59ea
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:37:21 -0600

    Create 20545.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20545.java
@@ -0,0 +1,5 @@
+/*
+Write an expression that computes the difference of two double variables salesSummer and salesSpring, which have been already declared and assigned values.
+*/
+
+salesSummer - salesSpring
