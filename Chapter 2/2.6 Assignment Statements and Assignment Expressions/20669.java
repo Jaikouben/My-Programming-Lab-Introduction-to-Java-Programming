61f6745166668598366175cba36f6953a87ee0aa
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:27:03 -0600

    Create 20669.java

---
+++ Chapter 2/2.6 Assignment Statements and Assignment Expressions/20669.java
@@ -0,0 +1,7 @@
+/*
+Given three already declared int variables, i, j, and temp, write some code that swaps the values in i and j. Use temp to hold the value of i and then assign j's value to i. The original value of i, which was saved in temp, can now be assigned to j.
+*/
+
+temp = i;
+i = j;
+j = temp;
