afe8442621785fef0f11a144623e5cc16ea076e0
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 14:59:03 -0600

    Create 20967.java

---
+++ Chapter 2/2.3 Reading Input from the Console/20967.java
@@ -0,0 +1,5 @@
+/*
+Write a statement that reads an integer value from standard input into val. Assume that val has already been declared as an int variable. Assume also that stdin is a variable that references a Scanner object associated with standard input.
+*/
+
+val = stdin.nextInt();
