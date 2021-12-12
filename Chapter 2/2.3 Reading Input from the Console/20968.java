87010713e7dee6667ab49ed261e1a0c6f9e52b45
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:11:06 -0600

    Create 20968.java

---
+++ Chapter 2/2.3 Reading Input from the Console/20968.java
@@ -0,0 +1,5 @@
+/*
+Write a statement that reads a floating point value from standard input into temperature. Assume that temperature. has already been declared as an double variable. Assume also that stdin is a variable that references a Scanner object associated with standard input.
+*/
+
+temperature = stdin.nextDouble();
