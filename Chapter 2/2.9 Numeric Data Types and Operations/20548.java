168b16abd247e7cbf582fccb045f72e51ff4f5a3
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:34:05 -0600

    Create 20548.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20548.java
@@ -0,0 +1,5 @@
+/*
+You are given two variables, already declared and assigned values, one of type double, named price, containing the price of an order, and the other of type int, named totalNumber, containing the number of orders. Write an expression that calculates the total price for all orders.
+*/
+
+price * totalNumber
