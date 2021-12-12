493f26893c4bc98a13ac48c1f67af820586253c5
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:22:37 -0600

    Create 20509.java

---
+++ Chapter 2/2.5 Variables/20509.java
@@ -0,0 +1,25 @@
+/*
+The exercise instructions here are LONG -- please read them all carefully. If you see an internal scrollbar to the right of these instructions, be sure to scroll down to read everything.
+
+Given an integer variable i and a floating-point variable f, that have already been given values, write a statement that writes both of their values to standard output in the following format:
+
+i=value-of-i f=value-of-f
+
+Thus, if i's value were 25 and f's value were 12.34, the output would be:
+
+i=25 f=12.34
+
+But you don't know what i's value and f's value are. They might be 187 and 24.06. If that's what their values are, the output from your statement should be:
+
+i=187 f=24.06
+
+On the other hand, they might be 19 and 2.001. If that's what their values are, the output from your statement should be:
+
+i=19 f=2.001
+
+Remember: you are GIVEN i and f-- that means they are already declared and they already have values! Don't change their values by assigning or initializing them! Just print them out the way we have shown above. Just write one statement to produce the output.
+
+Remember: in your output you must be displaying both the name of the variable (like i) and its value.
+*/
+
+System.out.println("i = " + i + " f = " + f);
