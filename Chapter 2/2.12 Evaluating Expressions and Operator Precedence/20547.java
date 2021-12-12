a2568b3d08f6f4b9e16ab748274384779ac98f69
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:49:38 -0600

    Create 20547.java

---
+++ Chapter 2/2.12 Evaluating Expressions and Operator Precedence/20547.java
@@ -0,0 +1,5 @@
+/*
+You are given two variables, already declared and assigned values, one of type double, named totalWeight, containing the weight of a shipment, and the other of type int, named quantity, containing the number of items in the shipment. Write an expression that calculates the weight of one item.
+*/
+
+totalWeight / quantity
