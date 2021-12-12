9ef4b260cf52ab8f843b575cb9aa72a64fadd116
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:23:12 -0600

    Create 20606.java

---
+++ Chapter 2/2.5 Variables/20606.java
@@ -0,0 +1,6 @@
+/*
+Given an int variable datum that has already been declared, write a few statements that read an integer value from standard input into this variable.
+*/
+
+Scanner input = new Scanner(System.in);
+datum = input.nextInt();
