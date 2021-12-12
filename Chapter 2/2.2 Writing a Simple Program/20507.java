967f91599f8897c0d73633bf1ff4c24eb712e5aa
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 14:55:14 -0600

    Create 20507.java

---
+++ Chapter 2/2.2 Writing a Simple Program/20507.java
@@ -0,0 +1,7 @@
+/*
+Given an integer variable count, write a statement that displays the value of count on the screen.
+
+Do not display anything else on the screen -- just the value of count.
+*/
+
+System.out.println(count);
