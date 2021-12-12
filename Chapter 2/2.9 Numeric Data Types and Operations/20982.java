043db134e57527b7f19fa414cf7f85b0c9e2d174
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:38:16 -0600

    Create 20982.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20982.java
@@ -0,0 +1,5 @@
+/*
+A wall has been built with two pieces of sheetrock, a smaller one and a larger one. The length of the smaller one is stored in the variable small. Similarly, the length of the larger one is stored in the variable large. Write a single expression whose value is the length of this wall.
+*/
+
+small + large
