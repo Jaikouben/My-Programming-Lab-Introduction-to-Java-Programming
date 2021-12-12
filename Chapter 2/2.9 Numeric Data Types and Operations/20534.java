f4cea00a19925ecec0d3bd85ecd4db6e6ac4a796
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:31:25 -0600

    Create 20534.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20534.java
@@ -0,0 +1,5 @@
+/*
+Given the variables costOfBusRental and maxBusRiders of type int , write an expression corresponding to the cost per rider (assuming the bus is full). (Do not worry about any fractional part of the expression-- let integer arithmetic, with truncation, act here.)
+*/
+
+costOfBusRental / maxBusRiders
