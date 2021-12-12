853f1edfe1f5cebfca0dba768f29b4f0998b246e
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:30:58 -0600

    Create 20530.java

---
+++ Chapter 2/2.9 Numeric Data Types and Operations/20530.java
@@ -0,0 +1,5 @@
+/*
+Given the variables taxablePurchases and taxFreePurchases (already declared and assigned values), write an expression corresponding to the total amount purchased.
+*/
+
+taxablePurchases + taxFreePurchases
