d762ac973cfe545ca6f710b577a814558f64c132
Arden <59106321+UnknownApparition@users.noreply.github.com>
2021-12-12 15:25:06 -0600

    Create 20670.java

---
+++ Chapter 2/2.6 Assignment Statements and Assignment Expressions/20670.java
@@ -0,0 +1,8 @@
+/*
+Given two int variables, firstPlaceWinner and secondPlaceWinner, write some code that swaps their values. Declare any additional variables as necessary.
+*/
+
+int swap1 = firstPlaceWinner;
+int swap2 = secondPlaceWinner;
+firstPlaceWinner = swap2;
+secondPlaceWinner = swap1;
