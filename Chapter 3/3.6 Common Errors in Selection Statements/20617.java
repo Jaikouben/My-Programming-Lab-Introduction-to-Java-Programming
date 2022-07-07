901a439d360e971dc6ee9097b38dd5d85abe298c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:21:43 -0500

    Create 20617.java

---
+++ Chapter 3/3.6 Common Errors in Selection Statements/20617.java
@@ -0,0 +1,7 @@
+/*
+Write a conditional that multiplies the value of the variable pay by one-and-a-half if the value of the boolean variable workedOvertime is true.
+*/
+
+if (workedOvertime == true){
+	pay *= 1.5;
+}
