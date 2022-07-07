a407c225927df0cb62f1fb535936f043f669d3f3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:22:35 -0500

    Create 20848.java

---
+++ Chapter 4/4.4 The String Type/20848.java
@@ -0,0 +1,5 @@
+/*
+Given three String variables that have been declared and given values, gold, silver, and bronze, write an expression whose value is the values of each these variables joined by a newline character. So if gold, silver, and bronze, had the values "Arakawa", "Cohen", and "Slutskaya", the expression, if it were printed would have the names "Arakawa", "Cohen", and "Slutskaya" each appearing on a separate line. (Do NOT print anything in this exercise: just write the expression.)
+*/
+
+gold + "\n" + silver + "\n" + bronze
