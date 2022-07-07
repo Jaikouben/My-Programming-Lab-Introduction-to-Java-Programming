0fad3b53aac9a796a756e8b1af92e20a9180d1e0
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-06 23:32:40 -0500

    Create 20665.java

---
+++ Chapter 3/3.2 boolean Data Type/20665.java
@@ -0,0 +1,5 @@
+/*
+Write an expression that evaluates to true if the value of the integer variable numberOfPrizes is divisible (with no remainder) by the integer variable numberOfParticipants. Assume that numberOfParticipants is not zero.
+*/
+
+numberOfPrizes % numberOfParticipants == 0
