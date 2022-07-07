aff52f787f09dbe568ab88d92f9b2b0b02e6880e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:23:32 -0500

    Create 20561.java

---
+++ Chapter 3/3.10 Logical Operators/20561.java
@@ -0,0 +1,5 @@
+/*
+Given that the variables x and y have already been declared and assigned values, write an expression that evaluates to true if x is non-negative and y is negative.
+*/
+
+x >= 0 && y < 0
