90295c169a976a7ab6bd42022f14d9fd1cca94df
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-06 23:19:32 -0500

    Create 20984.java

---
+++ Chapter 2/2.16 Numeric Type Conversions/20984.java
@@ -0,0 +1,5 @@
+/*
+Three classes of school children are selling tickets to the school play. The number of tickets sold by these classes, and the number of children in each of the classes have been read into these variables:tickets1, tickets2, tickets3 and class1, class2, class3. Write an expression for the average number of tickets sold per school child.
+*/
+
+(double) (tickets1 + tickets2 + tickets3) / (class1 + class2 + class3)
