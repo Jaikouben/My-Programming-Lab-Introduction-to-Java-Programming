64c60048bf17b9656d5a175ba0f03668de6937e5
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:29:33 -0500

    Create 20879.java

---
+++ Chapter 4/4.4 The String Type/20879.java
@@ -0,0 +1,5 @@
+/*
+Write an expression that results in a String consisting of the third through tenth characters of the String s.
+*/
+
+s.charAt(2) + s.charAt(3) + s.charAt(4) + s.charAt(5) + s.charAt(6) + s.charAt(7) + s.charAt(8) + s.charAt(9) + ""
