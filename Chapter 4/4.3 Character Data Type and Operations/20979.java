745fe4c6c1ecd4c7c9d4d68944d662ba5274a295
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:53:28 -0500

    Create 20979.java

---
+++ Chapter 4/4.3 Character Data Type and Operations/20979.java
@@ -0,0 +1,5 @@
+/*
+Two variables, num and cost have been declared and given values: num is an integer and cost is a double. Write a single statement that outputs num and cost to standard output. Print both values (num first, then cost), separated by a space on a single line that is terminated with a newline character. Do not output any thing else.
+*/
+
+System.out.println(num + " " + cost);
