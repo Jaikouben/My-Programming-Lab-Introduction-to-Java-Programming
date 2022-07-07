54c4866615af4bb3a8051e9ca70378eb6f7aecf5
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:20:39 -0500

    Create 20928.java

---
+++ Chapter 3/3.6 Common Errors in Selection Statements/20928.java
@@ -0,0 +1,9 @@
+/*
+Assume that isIsosceles is a boolean variable, and that the variables isoCount,triangleCount, and polygonCount have all been declared and initialized. Write a statement that adds 1 to each of these count variables (isoCount, triangleCount, and polygonCount) if isIsosceles is true.
+*/
+
+if (isIsosceles == true){
+	isoCount += 1;
+	triangleCount += 1;
+	polygonCount += 1;
+}
