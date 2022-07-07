65fffe6216867262b005203fe3b070d861a60b39
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-06 23:24:29 -0500

    Create 20990.java

---
+++ Chapter 2/2.18 Case Study: Counting Monetary Units/20990.java
@@ -0,0 +1,5 @@
+/*
+Assume that price is an integer variable whose value is the price (in US currency) in cents of an item. Assuming the item is paid for with a minimum amount of change and just single dollars, write an expression for the amount of change (in cents) that would have to be paid.
+*/
+
+price % 100
