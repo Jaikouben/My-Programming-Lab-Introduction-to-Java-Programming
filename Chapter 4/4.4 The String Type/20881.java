e1d1b5f8f0342f31c3f03d6950074f6d1696e069
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:36:57 -0500

    Create 20881.java

---
+++ Chapter 4/4.4 The String Type/20881.java
@@ -0,0 +1,5 @@
+/*
+Assume that given, middle and family are three variables of type String that have been assigned values. Write an expression whose value is a String consisting of the first character of given followed by a period followed by the first character of middle followed by a period followed by the first character of family followed by a period: in other words, the initials of the name. So if the values of these three variables were "John" "Fitzgerald" "Kennedy", then the expression's value would be "J.F.K.".
+*/
+
+given.charAt(0) + "." + middle.charAt(0) + "." + family.charAt(0) + "."
