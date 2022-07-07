94c7d58b9a2b7b36fa3258caa9a87f287d54a6bf
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:38:33 -0500

    Create 20873.java

---
+++ Chapter 4/4.4 The String Type/20873.java
@@ -0,0 +1,5 @@
+/*
+Given a String variable named sentence that has been initialized, write an expression whose value is the the very last character in the String referred to by sentence.
+*/
+
+sentence.charAt(sentence.length()-1)
