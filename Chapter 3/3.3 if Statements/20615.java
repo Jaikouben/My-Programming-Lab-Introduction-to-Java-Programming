012051e10199b13afc6715bc4e2131c234a8ff64
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:10:03 -0500

    Create 20615.java

---
+++ Chapter 3/3.3 if Statements/20615.java
@@ -0,0 +1,7 @@
+/*
+Write a conditional that assigns 10,000 to the variable bonus if the value of the variable goodsSold is greater than 500,000.
+*/
+
+if (goodsSold > 500000) {
+	bonus = 10000;
+}
