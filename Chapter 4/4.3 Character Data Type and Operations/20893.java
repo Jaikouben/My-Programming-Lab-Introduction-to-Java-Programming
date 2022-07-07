6eb06c29e5e4ba6fcb3246796c9a05af169eb2ca
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:56:03 -0500

    Create 20893.java

---
+++ Chapter 4/4.3 Character Data Type and Operations/20893.java
@@ -0,0 +1,5 @@
+/*
+Assume that c is a char variable has been declared and already given a value. Write an expression whose value is true if and only if c is what is called a whitespace character (that is a space or a tab or a newline-- none of which result in ink being printed on paper).
+*/
+
+c == ' ' || c == '\n' || c == '	'
