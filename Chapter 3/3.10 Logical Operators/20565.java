19d838c1a3cfd085e0185cce80c03a4d4d4d433d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:28:16 -0500

    Create 20565.java

---
+++ Chapter 3/3.10 Logical Operators/20565.java
@@ -0,0 +1,5 @@
+/*
+Given two variables, isEmpty of type boolean, indicating whether a class roster is empty or not, and numberOfCredits of type integer, containing the number of credits for a class, write an expression that evaluates to true if the class roster is not empty and the class is one or three credits.
+*/
+
+! isEmpty && (numberOfCredits == 1 || numberOfCredits == 3)
