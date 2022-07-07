54dc85318e6b936dbd4e0645eeb4e93ebe5e123f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:20:06 -0500

    Create 20922.java

---
+++ Chapter 3/3.6 Common Errors in Selection Statements/20922.java
@@ -0,0 +1,11 @@
+/*
+Assume that a boolean variable workedOvertime has been declared, and that another variable, hoursWorked has been declared and initialized. Write a statement that assigns the value true to workedOvertime if hoursWorked is greater than 40 and false otherwise.
+*/
+
+if (hoursWorked > 40){
+	workedOvertime = true;
+}
+
+else {
+	workedOvertime = false;
+}
