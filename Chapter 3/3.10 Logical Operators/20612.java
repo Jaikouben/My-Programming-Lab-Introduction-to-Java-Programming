1f7b417dfa6e05435534b67f48638175d49047d3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:33:51 -0500

    Create 20612.java

---
+++ Chapter 3/3.10 Logical Operators/20612.java
@@ -0,0 +1,5 @@
+/*
+Given the variables isFullTimeStudent and age, write an expression that evaluates to true if age is less than 19 or isFullTimeStudent is true.
+*/
+
+age < 19 || isFullTimeStudent == true
