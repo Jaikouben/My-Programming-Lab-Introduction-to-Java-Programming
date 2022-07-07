9e0243f23454dee84795a93f31874cb3cac7178c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-06 23:20:22 -0500

    Create 20985.java

---
+++ Chapter 2/2.16 Numeric Type Conversions/20985.java
@@ -0,0 +1,5 @@
+/*
+In mathematics, the Nth harmonic number is defined to be 1 + 1/2 + 1/3 + 1/4 + ... + 1/N. So, the first harmonic number is 1, the second is 1.5, the third is 1.83333... and so on. Write an expression whose value is the 8th harmonic number.
+*/
+
+(1.0 + 1.0/2.0 + 1.0/3.0 + 1.0/4.0 + 1.0/5.0 + 1.0/6.0 + 1.0/7.0 + 1.0/8.0)
