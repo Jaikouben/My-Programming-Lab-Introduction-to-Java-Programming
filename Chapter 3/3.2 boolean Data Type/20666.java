5f970a9ba6917db61a4dffb4f133e2e30153f1c8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-06 23:34:10 -0500

    Create 20666.java

---
+++ Chapter 3/3.2 boolean Data Type/20666.java
@@ -0,0 +1,5 @@
+/*
+Write an expression that evaluates to true if the value of the integer variable widthOfBox is not divisible by the value of the integer variable widthOfBook. Assume that widthOfBook is not zero. ("Not divisible" means has a remainder.)
+*/
+
+widthOfBox % widthOfBook != 0
