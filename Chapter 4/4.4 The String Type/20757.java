7f8ed009e51e7f65cb7fd913fa4069c2e51b292b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:07:54 -0500

    Create 20757.java

---
+++ Chapter 4/4.4 The String Type/20757.java
@@ -0,0 +1,10 @@
+/*
+There are two String variables, s1 and s2, that have already been declared and initialized. Write some code that exchanges their values.
+
+Declare any other variables as necessary.
+*/
+
+String temp;
+temp = s1;
+s1 = s2;
+s2 = temp;
