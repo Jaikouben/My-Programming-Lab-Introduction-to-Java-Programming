79e1117de575f2ded370d9febfe5f1a91c6bb7f9
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:35:42 -0500

    Create 20876.java

---
+++ Chapter 4/4.4 The String Type/20876.java
@@ -0,0 +1,5 @@
+/*
+Assume that word is a variable of type String that has been assigned a value. Write an expression whose value is a String consisting of the first three characters of the value of word. So if the value of word were "dystopia" the expression's value would be "dys".
+*/
+
+word.substring(0, 3)
