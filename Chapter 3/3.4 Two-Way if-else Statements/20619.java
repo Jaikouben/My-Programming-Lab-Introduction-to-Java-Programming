e1ca83f46d636ac59b8ba94f1c58249583792b03
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:12:51 -0500

    Create 20619.java

---
+++ Chapter 3/3.4 Two-Way if-else Statements/20619.java
@@ -0,0 +1,13 @@
+/*
+Write an if/else statement that compares the value of the variables soldYesterday and soldToday, and based upon that comparison assigns salesTrend the value -1 or 1.
+
+-1 represents the case where soldYesterday is greater than soldToday; 1 represents the case where soldYesterday is not greater than soldToday.
+*/
+
+if (soldYesterday > soldToday){
+	salesTrend = -1;
+}
+
+else {
+	salesTrend = 1;
+}
