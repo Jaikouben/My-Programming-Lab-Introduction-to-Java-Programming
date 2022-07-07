752202fecdbb1b877536ce27a5bd594b7163d4b3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:20:53 -0500

    Create 20850.java

---
+++ Chapter 4/4.4 The String Type/20850.java
@@ -0,0 +1,5 @@
+/*
+Given four int variables that have been declared and given values, octet1, octet2, octet3, and octet4, write a String expression whose value is the String equivalent of each these variables joined by a single period (.) So if octet1, octet2, octet3, and octet4, had the values 129, 42, 26, and 212 the expression's value would be "129.42.26.212". Alternatively, if octet1, octet2, octet3, and octet4, had the values 192, 168, 1and 44 the expression's value would be "192.168.1.44".
+*/
+
+octet1 + "." + octet2 + "." + octet3 + "." + octet4
