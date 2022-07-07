25d7934def877af8f070c8e79acd463430e2559e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:02:13 -0500

    Create 20900.java

---
+++ Chapter 4/4.3 Character Data Type and Operations/20900.java
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable has been declared and already given a value. Write an expression whose value is true if and only if x is alphanumeric, that is either a letter or a decimal digit.
+*/
+
+Character.isLetter(x) || Character.isDigit(x)
