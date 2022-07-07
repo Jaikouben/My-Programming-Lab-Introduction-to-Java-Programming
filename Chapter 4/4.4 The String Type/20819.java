ed318d7477b26de1829b6f6909770aeddfa43894
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:27:49 -0500

    Create 20819.java

---
+++ Chapter 4/4.4 The String Type/20819.java
@@ -0,0 +1,5 @@
+/*
+Given a String variable named sentence that has been initialized, write an expression whose value is the number of characters in the String referred to by sentence.
+*/
+
+sentence.length()
