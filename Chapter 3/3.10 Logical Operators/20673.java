bcc2fe05c12c12a49bb7d41f3d168d30d97f1a5d
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:34:52 -0500

    Create 20673.java

---
+++ Chapter 3/3.10 Logical Operators/20673.java
@@ -0,0 +1,11 @@
+/*
+Write a statement that toggles the value of onOffSwitch. That is, if onOffSwitch is false, its value is changed to true; if onOffSwitch is true, its value is changed to false.
+*/
+
+if (onOffSwitch == false){
+	onOffSwitch = true;
+}
+
+else if (onOffSwitch == true){
+	onOffSwitch = false;
+}
