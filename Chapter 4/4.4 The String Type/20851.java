b3d22c1de87ac16fcf35d03009efd6bef8bbe99e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:34:18 -0500

    Create 20851.java

---
+++ Chapter 4/4.4 The String Type/20851.java
@@ -0,0 +1,5 @@
+/*
+Given three String variables that have been declared and given values, firstName, middleName, and lastName, write an expression whose value is the initials of the three names: the first letter of each, joined together. So if firstName, middleName, and lastName, had the values "John", "Fitzgerald", and "Kennedy", the expression's value would be JFK". Alternatively, if firstName, middleName, and lastName, had the values "Franklin", "Delano", and "Roosevelt", the expression's value would be "FDR".
+*/
+
+"" + firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0)
