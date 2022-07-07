38adedc3a68ececf8c1c56b1801a142ae00f1bb8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:23:29 -0500

    Create 20978.java

---
+++ Chapter 4/4.4 The String Type/20978.java
@@ -0,0 +1,5 @@
+/*
+Assume that word is a String variable. Write a statement to display the message "Today's Word-Of-The-Day is: " followed by the value of word. The message and the value of word should appear together, on  a single line on standard output.
+*/
+
+System.out.println("Today's Word-Of-The-Day is: " + word);
