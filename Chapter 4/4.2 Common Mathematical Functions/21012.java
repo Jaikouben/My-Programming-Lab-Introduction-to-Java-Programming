c21832a6c51e58399c29f75087ea245e09be1484
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:50:42 -0500

    Create 21012.java

---
+++ Chapter 4/4.2 Common Mathematical Functions/21012.java
@@ -0,0 +1,5 @@
+/*
+The length of a rectangle is stored in a double variable named length, the width in one named width. Write an expression whose value is the length of the diagonal of the rectangle.
+*/
+
+Math.sqrt((length * length) + (width * width))
