e4519c3927724422bd0390807ef7d62ccaf51028
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:38:32 -0500

    Create 20768.java

---
+++ Chapter 3/3.14 Conditional Expressions/20768.java
@@ -0,0 +1,5 @@
+/*
+Four int variables, x1, x2, y1, and y2, have been declared and been given values. Write an expression whose value is the difference between the larger of x1 and x2 and the smaller of y1 and y2.
+*/
+
+(x1 < x2 ? x2 : x1) - (y1 < y2 ? y1 : y2)
