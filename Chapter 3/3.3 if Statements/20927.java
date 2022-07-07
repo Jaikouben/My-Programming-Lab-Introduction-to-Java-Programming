74ba904470336f5b151ecdcd0eb6f2010604f35f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:11:11 -0500

    Create 20927.java

---
+++ Chapter 3/3.3 if Statements/20927.java
@@ -0,0 +1,8 @@
+/*
+Assume that the variables gpa, deansList and studentName, have been declared and initialized. Write a statement that adds 1 to deansList and prints studentName to standard out if gpa exceeds 3.5.
+*/
+
+if (gpa > 3.5){
+	deansList += 1;
+	System.out.println(studentName);
+}
