dc40592c114524c300a3906c61c2c08639846c25
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:25:18 -0500

    Create 20971.java

---
+++ Chapter 4/4.4 The String Type/20971.java
@@ -0,0 +1,6 @@
+/*
+Assume that name has been declared suitably for storing names (like "Misha", "Emily" and "Sofia"). Assume also that stdin is a variable that references a Scanner object associated with standard input Write some code that reads a value into name then prints the message "Greetings, NAME" on a line by itself, where NAME is replaced the value that was read into name. For example, if your code read in "Rachel" it would print out "Greetings, Rachel" on a line by itself.
+*/
+
+name = stdin.nextLine();
+System.out.println("Greetings, " + name);
