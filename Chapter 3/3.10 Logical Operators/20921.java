a44f5aa1badd8151bff9cd1b9d2329cf602e79dd
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:29:20 -0500

    Create 20921.java

---
+++ Chapter 3/3.10 Logical Operators/20921.java
@@ -0,0 +1,11 @@
+/*
+Assume that a boolean variable isQuadrilateral has been declared, and that another variable, numberOfSides has been declared and initialized. Write a statement that assigns the value true if numberOfSides is exactly 4 and false otherwise.
+*/
+
+if (numberOfSides == 4){
+	isQuadrilateral = true;
+}
+
+else {
+	isQuadrilateral = false;
+}
