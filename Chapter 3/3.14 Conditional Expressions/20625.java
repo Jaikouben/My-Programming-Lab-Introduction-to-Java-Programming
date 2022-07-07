41e9fca48f7dbf45cf76715b98148fc9a8f9072b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:40:17 -0500

    Create 20625.java

---
+++ Chapter 3/3.14 Conditional Expressions/20625.java
@@ -0,0 +1,5 @@
+/*
+Write an expression using the conditional operator (? :) that compares the values of the variables x and y. The result (that is the value) of this expression should be the value of the larger of the two variables.
+*/
+
+(x > y) ? x : y
