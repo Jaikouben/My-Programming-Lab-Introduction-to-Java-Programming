c57edd890c38afbb6a72de58d6faa1552b0812c2
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:00:16 -0500

    Create 20897.java

---
+++ Chapter 4/4.3 Character Data Type and Operations/20897.java
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable has been declared and already given a value. Write an expression whose value is true if and only if x is a decimal digit (0-9).
+*/
+
+Character.isDigit(x)
