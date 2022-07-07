bdd2b346b9429dec65fce7c6fcfd91d719c9de9b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:22:31 -0500

    Create 20620.java

---
+++ Chapter 3/3.6 Common Errors in Selection Statements/20620.java
@@ -0,0 +1,11 @@
+/*
+Write an if/else statement that assigns true to the variable fever if the variable temperature is greater than 98.6; otherwise it assigns false to fever.
+*/
+
+if (temperature > 98.6){
+	fever = true;
+}
+
+else {
+	fever = false;
+}
