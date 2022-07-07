6c138f0149ed0de55d4d30da767e64fbdd11b0c2
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:31:06 -0500

    Create 20818.java

---
+++ Chapter 4/4.4 The String Type/20818.java
@@ -0,0 +1,9 @@
+/*
+Write an expression whose value is the number of characters in the following String:
+
+         "CRAZY!\n\\\t\\\\\\\\\\n. . . .\\ \\\r\007'\\'\"TOOMUCH!"
+
+Suggestion: copy/paste the above String into your code-- it's too crazy to try to copy it by typing.
+*/
+
+"CRAZY!\n\\\t\\\\\\\\\\n. . . .\\ \\\r\007'\\'\"TOOMUCH!".length()
