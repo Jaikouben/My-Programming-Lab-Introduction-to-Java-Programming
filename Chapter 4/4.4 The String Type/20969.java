a7460b767c651c7dd25830ab59193d844116fa76
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:17:47 -0500

    Create 20969.java

---
+++ Chapter 4/4.4 The String Type/20969.java
@@ -0,0 +1,5 @@
+/*
+Write a statement that reads a word from standard input into firstWord. Assume that firstWord. has already been declared as a String variable. Assume also that stdin is a variable that references a Scanner object associated with standard input.
+*/
+
+firstWord = stdin.nextLine();
