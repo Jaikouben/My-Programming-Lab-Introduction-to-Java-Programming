0890207b1c6c547a6fd6b8e8007593282dd05569
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:47:11 -0500

    Create 21013.java

---
+++ Chapter 4/4.2 Common Mathematical Functions/21013.java
@@ -0,0 +1,5 @@
+/*
+If a right triangle has sides of length A, B and C and if C is the largest, then it is called the "hypotenuse" and its length is the square root of the sum of the squares of the lengths of the shorter sides (A and B). Assume that variables a and b have been declared as doubles and that a and b contain the lengths of the shorter sides of a right triangle: write an expression for the length of the hypotenuse.
+*/
+
+Math.sqrt((a * a) + (b * b))
