27cacc098727bbe0b66c8c1bd7ec8a072caf04fa
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:26:53 -0500

    Create 20945.java

---
+++ Chapter 3/3.10 Logical Operators/20945.java
@@ -0,0 +1,7 @@
+/*
+Clunker Motors Inc. is recalling all vehicles from model years 2001-2006. Given a variable modelYear write a statement that prints the message "NO RECALL" to standard output if the value of modelYear DOES NOT fall within that range.
+*/
+
+if (modelYear < 2001 || modelYear > 2006){
+	System.out.println("NO RECALL");
+}
