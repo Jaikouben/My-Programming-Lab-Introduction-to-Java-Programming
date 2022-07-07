57178cd31dec862015105d1fc0c35a56ceab3009
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:21:05 -0500

    Create 20614.java

---
+++ Chapter 3/3.6 Common Errors in Selection Statements/20614.java
@@ -0,0 +1,7 @@
+/*
+Write a conditional that assigns the boolean value true to the variable fever if the variable temperature is greater than 98.6.
+*/
+
+if (temperature > 98.6){
+	fever = true;
+}
