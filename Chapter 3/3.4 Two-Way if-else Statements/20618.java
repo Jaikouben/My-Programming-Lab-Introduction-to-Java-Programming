cfa2b494d79e305ae5326d4548a7de5a9bcf696c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:12:19 -0500

    Create 20618.java

---
+++ Chapter 3/3.4 Two-Way if-else Statements/20618.java
@@ -0,0 +1,10 @@
+/*
+Write an if/else statement that compares the variable age with 65, adds 1 to the variable seniorCitizens if age is greater than or equal to 65, and adds 1 to the variable nonSeniors otherwise.
+*/
+
+if (age >= 65){
+	seniorCitizens += 1;
+}
+else {
+	nonSeniors += 1;
+}
