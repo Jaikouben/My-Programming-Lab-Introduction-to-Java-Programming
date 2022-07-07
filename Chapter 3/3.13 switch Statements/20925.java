daaf43084bea2908e2d2b5b9efa7bb299bca5bb8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:36:51 -0500

    Create 20925.java

---
+++ Chapter 3/3.13 switch Statements/20925.java
@@ -0,0 +1,58 @@
+/*
+In the Happy Valley School System, children are classified by age as follows:
+
+less than 2, ineligible
+2, toddler
+3-5, early childhood
+6-7, young reader
+8-10, elementary
+11 and 12, middle
+13, impossible
+14-16, high school
+17-18, scholar
+greater than 18, ineligible
+Given an int variable age, write a switch statement that prints out, on a line by itself, the appropriate label from the above list based on age.
+*/
+
+switch (age){
+	case 0: System.out.println("ineligible");
+		break;
+	case 1: System.out.println("ineligible");
+		break;
+	case 2: System.out.println("toddler");
+		break;
+	case 3: System.out.println("early childhood");
+		break;
+	case 4: System.out.println("early childhood");
+		break;
+	case 5: System.out.println("early childhood");
+		break;
+	case 6: System.out.println("young reader");
+		break;
+	case 7: System.out.println("young reader");
+		break;
+	case 8: System.out.println("elementary");
+		break;
+	case 9: System.out.println("elementary");
+		break;
+	case 10: System.out.println("elementary");
+		break;
+	case 11: System.out.println("middle");
+		break;
+	case 12: System.out.println("middle");
+		break;
+	case 13: System.out.println("impossible");
+		break;
+	case 14: System.out.println("high school");
+		break;
+	case 15: System.out.println("high school");
+		break;
+	case 16: System.out.println("high school");
+		break;
+	case 17: System.out.println("scholar");
+		break;
+	case 18: System.out.println("scholar");
+		break;
+	default: System.out.println("ineligible");
+	
+}
