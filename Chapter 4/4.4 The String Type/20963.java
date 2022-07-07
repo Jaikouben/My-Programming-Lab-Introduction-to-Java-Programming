7f52e795cb90398f9b7eef2beea9dc7615b9062c
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:19:16 -0500

    Create 20963.java

---
+++ Chapter 4/4.4 The String Type/20963.java
@@ -0,0 +1,5 @@
+/*
+Given a String variable word, write a String expression that parenthesizes the value of word. So, if word contains "sadly", the value of the expression would be the String "(sadly)"
+*/
+
+"(" + word + ")"
