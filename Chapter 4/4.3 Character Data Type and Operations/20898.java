ccbeafac4181671eeb22f84dbf658421ad064993
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:00:55 -0500

    Create 20898.java

---
+++ Chapter 4/4.3 Character Data Type and Operations/20898.java
@@ -0,0 +1,5 @@
+/*
+Assume that x is a char variable has been declared and already given a value. Write an expression whose value is true if and only if x is an octal (Base 8) digit (0-7).
+*/
+
+x >= '0' && x <= '7'
