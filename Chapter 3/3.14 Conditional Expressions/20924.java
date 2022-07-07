2185bfb86cc365f941eb98d25cdbcd84525add44
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:39:49 -0500

    Create 20924.java

---
+++ Chapter 3/3.14 Conditional Expressions/20924.java
@@ -0,0 +1,5 @@
+/*
+Assume that credits is an int variable whose value is 0 or positive. Write an expression whose value is "freshman" or "sophomore" or "junior" or "senior" based on the value of credits. In particular: if the value of credits is less than 30 the expression's value is "freshman"; 30-59 would be a "sophomore", 60-89 would be "junior" and 90 or more would be a "senior".
+*/
+
+(credits < 30) ? "freshman" : (credits >= 30 && credits <= 59) ? "sophomore" : (credits >= 60 && credits <= 89) ? "junior" : "senior"
