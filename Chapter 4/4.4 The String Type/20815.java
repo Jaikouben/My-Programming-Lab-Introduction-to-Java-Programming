d533ab9134974fbd646dbd17e326028003f71cd8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:20:27 -0500

    Create 20815.java

---
+++ Chapter 4/4.4 The String Type/20815.java
@@ -0,0 +1,7 @@
+/*
+Write the declaration of two String variable named background and selectionColor and initialize them to "white" and "blue" respectively.
+*/
+
+String background, selectionColor;
+background = "white";
+selectionColor = "blue";
