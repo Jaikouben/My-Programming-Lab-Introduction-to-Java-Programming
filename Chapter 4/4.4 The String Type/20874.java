022a4d1e2e4f932b90be30b58157bf3ad9d845f4
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:32:02 -0500

    Create 20874.java

---
+++ Chapter 4/4.4 The String Type/20874.java
@@ -0,0 +1,5 @@
+/*
+Assume that word is a variable of type String that has been assigned a value. Write an expression whose value is a String consisting of the last three characters of the value of word. So if the value of word were "biggest" the expression's value would be "est".
+*/
+
+word.substring(word.length() - 3)
