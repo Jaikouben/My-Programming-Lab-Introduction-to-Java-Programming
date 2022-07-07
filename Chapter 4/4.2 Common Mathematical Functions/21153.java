24bc6fe9b8dc357340f24064a7b6af2d7ebd3823
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:48:07 -0500

    Create 21153.java

---
+++ Chapter 4/4.2 Common Mathematical Functions/21153.java
@@ -0,0 +1,9 @@
+/*
+The Math class provides a static method, max, that accepts two int arguments and returns the value of the larger one.
+
+Two int variables,population1 and population2, have already been declared and initialized.
+
+Write an expression (not a statement!) whose value is the larger of population1 and population2 by calling max.
+*/
+
+Math.max(population1, population2)
