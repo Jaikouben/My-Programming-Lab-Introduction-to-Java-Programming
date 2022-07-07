e42c27c660df478fd79f1f877773c39640b3ab24
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:16:04 -0500

    Create 20849.java

---
+++ Chapter 4/4.4 The String Type/20849.java
@@ -0,0 +1,5 @@
+/*
+Given three int variables that have been declared and given values, areaCode, exchange, and lastFour, write a String expression whose value is the String equivalent of each these variables joined by a single hyphen (-) So if areaCode, exchange, and lastFour, had the values 800, 555, and 1212, the expression's value would be "800-555-1212". Alternatively, if areaCode, exchange, and lastFour, had the values 212, 867 and 5309 the expression's value would be "212-867-5309".
+*/
+
+areaCode + "-" + exchange + "-" + lastFour
