7e1966ac089f515b8801edca7bb75df43f8c987f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 01:32:50 -0500

    Create 20822.java

---
+++ Chapter 4/4.4 The String Type/20822.java
@@ -0,0 +1,5 @@
+/*
+Assume that name is a variable of type String that has been assigned a value. Write an expression whose value is the first character of the value of name. So if the value of name were "Smith" the expression's value would be 'S'.
+*/
+
+name.charAt(0)
