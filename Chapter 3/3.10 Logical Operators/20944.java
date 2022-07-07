96ad2864d10cda9862d56c13744d69e66bad830b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-07 00:32:04 -0500

    Create 20944.java

---
+++ Chapter 3/3.10 Logical Operators/20944.java
@@ -0,0 +1,13 @@
+/*
+Clunker Motors Inc. is recalling all vehicles from model years 2001-2006. A boolean variable named recalled has been declared. Given a variable modelYear write a statement that assigns true to recalled if the value of modelYear falls within the recall range and assigns false otherwise.
+
+Do not use an if statement in this exercise!
+*/
+
+if (modelYear >= 2001 && modelYear <= 2006){
+	recalled = true;
+}
+
+else {
+	recalled = false;
+}
