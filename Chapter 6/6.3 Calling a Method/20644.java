08f14b3616de5fdf384560fec470debb19df3547
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:38:16 -0500

    Create 20644.java

---
+++ Chapter 6/6.3 Calling a Method/20644.java
@@ -0,0 +1,9 @@
+/*
+printErrorDescription is a method that accepts one int argument and returns no value.
+
+Write a statement that invokes the method printErrorDescription, passing it the value 14.
+
+Assume that printErrorDescription is defined in the same class that calls it.
+*/
+
+printErrorDescription(14);
