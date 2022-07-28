e0949ecbdaabc62149ddaa25d13bf8407455a635
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:39:02 -0500

    Create 21015.java

---
+++ Chapter 6/6.3 Calling a Method/21015.java
@@ -0,0 +1,9 @@
+/*
+Assume that x is a variable that has been declared as an int and been given a value. Assume that twice is a method (in the same class as the caller) that receives a single integer parameter and returns twice its value. (So if you pass 7 to twice it will return 14. Thus the expression twice(7) has the value 14.
+
+Write an expression whose value is eight times that of x without using the standard Java arithmetic operators (*,+, etc.). Instead, use calls to twice to accomplish this.
+
+In this exercise you must write this as a single expression-- you must not write any statements. Also, you may only use the twice() function-- no other functions or operators.
+*/
+
+twice(twice(twice(x)))
