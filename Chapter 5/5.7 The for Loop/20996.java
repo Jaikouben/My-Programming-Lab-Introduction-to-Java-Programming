a24ccc9fa00f1442cb1ee1fd66697cc0d5fe74e7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:20:17 -0500

    Create 20996.java

---
+++ Chapter 5/5.7 The for Loop/20996.java
@@ -0,0 +1,7 @@
+/*
+Write a for loop that prints the integers 50 through 1, separated by spaces. Use no variables other than count.
+*/
+
+for (int count = 50; count >= 1; count--){
+	System.out.print(count + " ");
+}
