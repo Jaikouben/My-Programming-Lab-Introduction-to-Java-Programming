e52a72338c6c4f4f9f093231ebeba6757edd8f39
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:41:51 -0500

    Create 20840.java

---
+++ Chapter 6/6.3 Calling a Method/20840.java
@@ -0,0 +1,7 @@
+/*
+Write a method, makeSubjectLine, that gets a String argument and returns the same String but with "Subject: " in front of it. So if the argument is "Are you going to the show?" the method returns "Subject: Are you going to the show?".
+*/
+
+public String makeSubjectLine(String text) { 
+    return "Subject: " + text; 
+}
