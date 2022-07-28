062c2f2d1b585121636e84df8d566e8a1cc9ebb7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:20:48 -0500

    Create 20686.java

---
+++ Chapter 5/5.7 The for Loop/20686.java
@@ -0,0 +1,12 @@
+/*
+Assume the int variables i and result have been declared but not initialized. Write a for loop header, i.e. something of the form
+
+for (  .   .   .   )
+for the following loop body:
+
+result = result * i;
+When the loop terminates, result should hold the product of the odd numbers between 10 and 20.
+*/
+
+result = 1;
+for (i = 11; i < 20; i+= 2)
