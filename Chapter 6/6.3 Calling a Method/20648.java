f30f3738837b9c949a29be5920c2757596c3ac04
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:37:41 -0500

    Create 20648.java

---
+++ Chapter 6/6.3 Calling a Method/20648.java
@@ -0,0 +1,11 @@
+/*
+max is a method that accepts two int arguments and returns the value of the larger one.
+
+Two int variables, population1 and population2, have already been declared and initialized.
+
+Write an expression (not a statement!) whose value is the larger of population1 and population2 by calling max.
+
+Assume that max is defined in the same class that calls it.
+*/
+
+max(population1, population2)
