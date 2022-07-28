63f180974f0b476852f199c1bb960181b4ecc628
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:06:35 -0500

    Create 21214.java

---
+++ Chapter 5/5.2 The while Loop/21214.java
@@ -0,0 +1,7 @@
+/*
+Given a Scanner reference variable named input that has been associated with an input source consisting of a sequence of lines, write the code necessary to read in every line and print it out on its own line.
+*/
+
+while (input.hasNext()){
+	System.out.println(input.nextLine());
+}
