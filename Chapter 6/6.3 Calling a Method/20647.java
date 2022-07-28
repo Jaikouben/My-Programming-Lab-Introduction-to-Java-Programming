4413993a1a468ecb23d38191232f388f47aa980e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:34:55 -0500

    Create 20647.java

---
+++ Chapter 6/6.3 Calling a Method/20647.java
@@ -0,0 +1,11 @@
+/*
+toThePowerOf is a method that accepts two int arguments and returns the value of the first parameter raised to the power of the second.
+
+An int variable cubeSide has already been declared and initialized. Another int variable, cubeVolume, has already been declared.
+
+Write a statement that calls toThePowerOf to compute the value of cubeSide raised to the power of 3 and that stores this value in cubeVolume.
+
+Assume that toThePowerOf is defined in the same class that calls it.
+*/
+
+cubeVolume = toThePowerOf(cubeSide, 3);
