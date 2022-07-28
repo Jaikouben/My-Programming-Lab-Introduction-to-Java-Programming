e3074d110ac290b759dac9b345c46377fe94fecd
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:41:12 -0500

    Create 20576.java

---
+++ Chapter 6/6.3 Calling a Method/20576.java
@@ -0,0 +1,6 @@
+/*
+Write the code for invoking a method named sendTwo. There are two arguments for this method: a double and an int. Invoke the method with the double value of 15.955 and the int value of 133.
+Assume that sendTwo is defined in the same class that calls it.
+*/
+
+sendTwo(15.955, 133);
