b48ec55036f250778c4efbccfdd91b9029367e10
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:12:30 -0500

    Create 20682.java

---
+++ Chapter 5/5.6 The do-while Loop/20682.java
@@ -0,0 +1,12 @@
+/*
+Given an int variable k that has already been declared, use a do...while loop to print a single line consisting of 53 asterisks. Use no variables other than k.
+*/
+
+k = 53;
+
+do {
+	System.out.print("*");
+	k--;
+}
+
+while (k > 0);
