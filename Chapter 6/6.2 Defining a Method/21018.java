3bf00849c1ee541255d4af9bb7d5228200937db7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:32:47 -0500

    Create 21018.java

---
+++ Chapter 6/6.2 Defining a Method/21018.java
@@ -0,0 +1,8 @@
+/*
+Given that a method receives three parameters a, b, c, of type double, write some code, to be included as part of the method, that checks to see if the value of a is 0; if it is, the code prints the message "no solution for a=0" and returns from the method.
+*/
+
+if (a == 0){
+	System.out.println("no solution for a=0");
+	return;	
+}
