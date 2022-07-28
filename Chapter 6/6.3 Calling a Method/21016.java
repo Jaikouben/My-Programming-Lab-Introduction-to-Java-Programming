9607b8da595fe93ee124319a413473deaec96fa3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:40:05 -0500

    Create 21016.java

---
+++ Chapter 6/6.3 Calling a Method/21016.java
@@ -0,0 +1,9 @@
+/*
+Assume that x is a variable that has been declared as an int and been given a value. Assume that oneMore is a function that receives a single integer parameter and returns a value that is one greater than its argument. (So if you pass 7 to oneMore it will return 8. Thus, the expression oneMore(9) has the value 10.
+
+Write an expression whose value is four more than x without using the standard Java arithmetic operators (*,+, etc.). Instead, use calls to oneMore to accomplish this.
+
+In this exercise you must write this as a single expression-- you must not write any statements. Also, you may only use the oneMore function-- no other functions or operators.
+*/
+
+oneMore(oneMore(oneMore(oneMore(x))))
