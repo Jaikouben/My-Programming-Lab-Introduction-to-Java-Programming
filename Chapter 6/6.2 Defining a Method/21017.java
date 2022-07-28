0b4b291a5433170f77c43ee8a9be32a9ed213a3e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:33:07 -0500

    Create 21017.java

---
+++ Chapter 6/6.2 Defining a Method/21017.java
@@ -0,0 +1,7 @@
+/*
+Given that a method receives three parameters a, b, c, of type double, write some code, to be included as part of the method, that determines whether the value of "b squared" – 4ac is negative. If negative, the code prints out the message "no real solutions" and returns from the method
+*/
+
+if ((b * b) - (4 * a * c) < 0){
+	System.out.println("no real solutions");
+}
