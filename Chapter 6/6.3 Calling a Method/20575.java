a99eb288b9cb0134ee0637ebb26b29d970885db5
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:40:44 -0500

    Create 20575.java

---
+++ Chapter 6/6.3 Calling a Method/20575.java
@@ -0,0 +1,7 @@
+/*
+Write the code for invoking a method named sendVariable. There is one int argument for this method.
+Assume that an int variable called x has already been declared and initialized to some value. Use this variable's value as an argument in your method invocation.
+Assume that sendVariable is defined in the same class that calls it.
+*/
+
+sendVariable(x);
