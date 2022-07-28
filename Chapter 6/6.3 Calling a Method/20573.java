61aa1945c35610f83e07851da7bfa6dbe3ed6c21
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:33:44 -0500

    Create 20573.java

---
+++ Chapter 6/6.3 Calling a Method/20573.java
@@ -0,0 +1,6 @@
+/*
+Write the code for invoking a method named sendSignal. There are no arguments for this method.
+Assume that sendSignal is defined in the same class that calls it.
+*/
+
+sendSignal();
