2a203f36570cc0863d187950b22eda3449ceff22
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:32:02 -0500

    Create 20661.java

---
+++ Chapter 6/6.2 Defining a Method/20661.java
@@ -0,0 +1,13 @@
+/*
+Write the definition of a method min that has two int parameters and returns the smaller.
+*/
+
+public static int min(int var1, int var2){
+	if (var1 < var2){
+		return var1;
+	}
+	
+	else {
+		return var2;
+	}
+}
