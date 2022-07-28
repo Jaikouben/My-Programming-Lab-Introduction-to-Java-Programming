80d166ffa87d122ab7f9f1443451058aae73d375
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:21:42 -0500

    Create 20687.java

---
+++ Chapter 5/5.7 The for Loop/20687.java
@@ -0,0 +1,11 @@
+/*
+Assume the int variables i,lo, hi, and result have been declared and that lo and hi have been initialized. Assume further that result has been initialized to the value 0.
+
+Write a for loop that adds the integers from lo up through hi (inclusive), and stores the result in result.
+
+Your code should not change the values of lo and hi. Also, do not declare any additional variables -- use only i,lo, hi, and result.
+*/
+
+for (i = lo; i <= hi; i++){
+	result = result + i;
+}
