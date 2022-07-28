887e6c27bccec2b9e6a6aecf369c834c7cb43376
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:34:04 -0500

    Create 20557.java

---
+++ Chapter 6/6.3 Calling a Method/20557.java
@@ -0,0 +1,7 @@
+/*
+Write the code for invoking a method named sendObject. There is one argument for this method which is of type Customer.
+Assume that there is a reference to an object of type Customer, in a variable called John_Doe. Use this reference as your argument.
+Assume that sendObject is defined in the same class that calls it.
+*/
+
+sendObject(John_Doe);
