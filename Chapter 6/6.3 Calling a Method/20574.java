4dc68b5cf917af19e5bfbe078389161f86127e73
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:40:26 -0500

    Create 20574.java

---
+++ Chapter 6/6.3 Calling a Method/20574.java
@@ -0,0 +1,6 @@
+/*
+Write the code for invoking a method named sendNumber . There is one int argument for this method. Send the number 5 as an argument to this method.
+Assume that sendNumber is defined in the same class that calls it.
+*/
+
+sendNumber(5);
