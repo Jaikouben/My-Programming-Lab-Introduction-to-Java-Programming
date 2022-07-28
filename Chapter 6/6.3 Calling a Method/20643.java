14288d05bee6e3bd64e3dbb6ac82a0d199e9b731
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:39:26 -0500

    Create 20643.java

---
+++ Chapter 6/6.3 Calling a Method/20643.java
@@ -0,0 +1,7 @@
+/*
+printTodaysDate is a method that accepts no arguments and returns no value. Write a statement that calls printTodaysDate.
+
+Assume that printTodaysDate is defined in the same class that calls it.
+*/
+
+printTodaysDate();
