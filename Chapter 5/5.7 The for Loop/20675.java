1d28e58038c71afadead20e1c158674bbe77cece
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:19:49 -0500

    Create 20675.java

---
+++ Chapter 5/5.7 The for Loop/20675.java
@@ -0,0 +1,7 @@
+/*
+Given an int variable n that has already been declared and initialized to a positive value, and another int variable j that has already been declared, use a for loop to print a single line consisting of n asterisks. Thus if n contains 5, five asterisks will be printed. Use no variables other than n and j.    
+*/
+
+for (j = 0; j < n; j++){
+	System.out.print("*");
+}
