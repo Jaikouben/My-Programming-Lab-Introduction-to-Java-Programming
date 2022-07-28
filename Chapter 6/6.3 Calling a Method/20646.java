64fe26939d93592516265e3e112591b5912e556b
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:34:33 -0500

    Create 20646.java

---
+++ Chapter 6/6.3 Calling a Method/20646.java
@@ -0,0 +1,11 @@
+/*
+add is a method that accepts two int arguments and returns their sum.
+
+Two int variables, euroSales and asiaSales, have already been declared and initialized. Another int variable, eurasiaSales, has already been declared.
+
+Write a statement that calls add to compute the sum of euroSales and asiaSales and that stores this value in eurasiaSales.
+
+Assume that add is defined in the same class that calls it.
+*/
+
+eurasiaSales = add(euroSales, asiaSales);
