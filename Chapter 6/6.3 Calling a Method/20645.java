cc5f4f7a3f6f68b274bd325baa3f1e8b2d027828
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:39:48 -0500

    Create 20645.java

---
+++ Chapter 6/6.3 Calling a Method/20645.java
@@ -0,0 +1,11 @@
+/*
+printLarger is a method that accepts two int arguments and returns no value.
+
+Two int variables, sales1 and sales2, have already been declared and initialized.
+
+Write a statement that calls printLarger, passing it sales1 and sales2.
+
+Assume that printLarger is defined in the same class that calls it.
+*/
+
+printLarger(sales1, sales2);
