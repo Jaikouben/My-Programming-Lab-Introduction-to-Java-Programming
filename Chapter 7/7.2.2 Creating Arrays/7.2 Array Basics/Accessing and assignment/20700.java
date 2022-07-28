02a480fc9d1a0a11ced39714c7f9f5dc098d3710
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 14:00:57 -0500

    Create 20700.java

---
+++ Chapter 7/7.2.2 Creating Arrays/7.2 Array Basics/Accessing and assignment/20700.java
@@ -0,0 +1,7 @@
+/*
+Assume that an array named salarySteps whose elements are of type int and that has exactly five elements has already been declared.
+
+Write a single statement to assign the value 30000 to the first element of this array.
+*/
+
+salarySteps[0] = 30000;
