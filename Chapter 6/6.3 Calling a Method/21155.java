cfbef2cada5dc44945bcfe9fbf99d4f2e46b43b6
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:41:32 -0500

    Create 21155.java

---
+++ Chapter 6/6.3 Calling a Method/21155.java
@@ -0,0 +1,9 @@
+/*
+printStars is a method that accepts a single int argument and returns no value. It prints as many stars as indicated by its argument.
+
+Given a variable x of type double that has been given a value, write a statement that invokes printStars passing x as an argument.
+
+Assume that printStars is defined in the same class that calls it.
+*/
+
+printStars(x);
