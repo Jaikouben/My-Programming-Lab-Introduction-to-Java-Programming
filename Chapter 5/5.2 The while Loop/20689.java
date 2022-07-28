379731bc8e6081ca73d7d6a0b37afe2a5e29ec15
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-07-28 13:03:19 -0500

    Create 20689.java

---
+++ Chapter 5/5.2 The while Loop/20689.java
@@ -0,0 +1,5 @@
+/*
+Write a statement that increases the value of the int variable total by the value of the int variable amount. That is, add the value of amount to total and assign the result to total.
+*/
+
+total += amount;
