290bfd815e05f8509112e51b5ad463b55588cb24
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 16:59:58 -0500

    Create 20709.java

---
+++ Chapter 7/7.2 Array Basics/Accessing and assignment/20709.java
@@ -0,0 +1,5 @@
+/*
+Assume that an array of integers named a has been declared and initialized. Write a single statement that assigns a new value to the first element of the array. The new value should be equal to twice the value stored in the last element of the array.
+*/
+
+a[0] = a[a.length-1] * 2;
