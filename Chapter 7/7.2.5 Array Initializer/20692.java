9d8c0fc6c73ec497a55c0bf84abe74a3ecbdf7cb
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:06:37 -0500

    Create 20692.java

---
+++ Chapter 7/7.2.5 Array Initializer/20692.java
@@ -0,0 +1,5 @@
+/*
+In a single   statement: declare, create and initialize an array named a of   ten  elements of type int with the values of the elements (starting with the first) set to 10, 20, ..., 100 respectively.
+*/
+
+int[] a = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
