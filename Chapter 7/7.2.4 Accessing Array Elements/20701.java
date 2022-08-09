7c2b237b638c0daed4d621053157a0e802fd31a3
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:03:14 -0500

    Create 20701.java

---
+++ Chapter 7/7.2.4 Accessing Array Elements/20701.java
@@ -0,0 +1,7 @@
+/*
+Assume that an array of integers named salarySteps that contains exactly five elements has been declared.
+
+Write a statement that assigns the value 160000 to the last element of the array salarySteps.
+*/
+
+salarySteps[salarySteps.length-1] = 160000;
