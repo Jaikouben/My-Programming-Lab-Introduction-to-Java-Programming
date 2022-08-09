90cada3928ab2da6e68efca6fea8961b9539c469
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:06:57 -0500

    Create 20694.java

---
+++ Chapter 7/7.2.5 Array Initializer/20694.java
@@ -0,0 +1,7 @@
+/*
+Write a statement to declare and initialize an array named denominations that contains exactly six  elements of type of int.
+
+Your declaration statement should initialize the elements of the array to the following values: 1, 5, 10, 25, 50, 100. (The value 1 goes into the first element, the value 100 to the last.)
+*/
+
+int[] denominations = {1, 5, 10, 25, 50, 100};
