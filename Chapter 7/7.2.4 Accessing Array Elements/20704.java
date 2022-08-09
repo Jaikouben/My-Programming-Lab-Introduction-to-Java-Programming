5b2136fc52841c01f8b35ba662e6fad7c2772848
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:03:39 -0500

    Create 20704.java

---
+++ Chapter 7/7.2.4 Accessing Array Elements/20704.java
@@ -0,0 +1,5 @@
+/*
+Assume that an array of ints named a has been declared with 12 elements. The integer variable k holds a value between 0 and 6. Assign 15 to the array element whose index is k.
+*/
+
+a[k] = 15;
