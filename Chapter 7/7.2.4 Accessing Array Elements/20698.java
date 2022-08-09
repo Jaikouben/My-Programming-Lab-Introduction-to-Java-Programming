8dafab145f0f362d8de3006a96e0e6e72ac264f7
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:05:40 -0500

    Create 20698.java

---
+++ Chapter 7/7.2.4 Accessing Array Elements/20698.java
@@ -0,0 +1,7 @@
+/*
+Given that the array monthSales of integers has already been declared and that its elements contain sales data for the 12 months of the year in order (i.e., January, February, etc.), write a statement that writes to standard output the element corresponding to October.
+
+Do not write anything else out to standard output.
+*/
+
+System.out.println(monthSales[9]);
