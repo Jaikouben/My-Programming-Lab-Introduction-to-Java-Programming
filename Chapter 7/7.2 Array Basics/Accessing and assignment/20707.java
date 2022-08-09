a2e2ff565f58f57c48c6c274016ca16ea3aa708f
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 16:57:09 -0500

    Create 20707.java

---
+++ Chapter 7/7.2 Array Basics/Accessing and assignment/20707.java
@@ -0,0 +1,5 @@
+/*
+An array of ints named a has been declared with 12 elements. The integer variable k holds a value between 2 and 8. Assign 22 to the element just before a[k].
+*/
+
+a[k - 1] = 22;
