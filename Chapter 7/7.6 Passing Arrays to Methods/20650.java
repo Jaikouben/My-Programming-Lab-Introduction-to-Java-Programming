5f7a37c91995fb19b1399a3ad0d78d4d2f1f4982
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:10:20 -0500

    Create 20650.java

---
+++ Chapter 7/7.6 Passing Arrays to Methods/20650.java
@@ -0,0 +1,9 @@
+/*
+printArray is a method that accepts one argument, an array of ints. The method prints the contents of the array; it does not return a value.
+
+inventory is an array of ints that has been already declared and filled with values.
+
+Write a statement that prints the contents of the array inventory by calling the method printArray.
+*/
+
+printArray(inventory);
