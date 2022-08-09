f0b1ebc66968d5df5fe761653da96e9ce51a5810
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:00:34 -0500

    Create 20697.java

---
+++ Chapter 7/7.2 Array Basics/Accessing and assignment/20697.java
@@ -0,0 +1,5 @@
+/*
+Assume that the array arr has been declared. Write a statement that assigns the next to last   element of the array to the variable x, which has already been declared.
+*/
+
+x = arr[arr.length-2];
