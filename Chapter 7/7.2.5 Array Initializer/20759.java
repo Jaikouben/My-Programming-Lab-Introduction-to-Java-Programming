7bcb0c28851f6642697057ceeff7ac3f2c9d2534
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:07:21 -0500

    Create 20759.java

---
+++ Chapter 7/7.2.5 Array Initializer/20759.java
@@ -0,0 +1,5 @@
+/*
+Declare an array reference variable, week, and initialize it to an array containing the strings "mon", "tue", "wed", "thu", "fri", "sat", "sun" (in that order).
+*/
+
+String[] week = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
