a1bde958e56585cd93db3961eeeb8c31abb0e771
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-08-09 17:04:58 -0500

    Create 20672.java

---
+++ Chapter 7/7.2.4 Accessing Array Elements/20672.java
@@ -0,0 +1,7 @@
+/*
+Given an array arr, of type int, along with two int variables i and j, write some code that swaps the values of arr[i] and arr[j]. Declare any additional variables as necessary.
+*/
+
+int temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
